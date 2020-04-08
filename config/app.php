f24c25606aa8b9c5aedb6a53d50b7fb14e0f84b9
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-04-09 05:34:03 +0700

    add provider and alias in app.php config

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // 3rd party
+        Maatwebsite\Excel\ExcelServiceProvider::class
 
     ],
 
@@ -225,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class
 
     ],
 
