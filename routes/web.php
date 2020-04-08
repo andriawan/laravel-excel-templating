85b3fd82b37ab5f68a462c6ee30895b2f3598af6
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-04-09 06:25:35 +0700

    add sample route for excel generate

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/store-to-server', function () {
+
+    Excel::create('tester', function($excel) {
+        $excel->sheet('test-sheet', function($sheet) {
+        });
+    })->store('xls');
+    
+});
+
+Route::get('/save-to-browser', function () {
+
+    Excel::create('tester', function($excel) {
+        $excel->sheet('test-sheet', function($sheet) {
+        });
+    })->export('xls');
+    
+});
+
+Route::get('/load-from-template', function () {
+
+    $file = storage_path('templates/templates.xlsx');
+
+    Excel::load($file, function($excel) {
+
+        // Sheet1 is your sheet name
+        $excel->sheet('Sheet1', function($sheet) {
+
+            for ($i=0; $i < 100000; $i++) { 
+
+                // copy style from templates
+                $sheet->duplicateStyle(
+                    $sheet->getStyle(
+                        'A2:C2'),'A' . ($i + 2) . ':C' . ($i + 2)
+                    );
+
+                $sheet->SetCellValue('A' . ($i + 2), 'test ' . $i);
+                $sheet->SetCellValue('B' . ($i + 2), 'test ' . $i);
+                $sheet->SetCellValue('C' . ($i + 2), 'test ' . $i);
+                
+            }
+        });
+    })->export('xls');
+    
 });
